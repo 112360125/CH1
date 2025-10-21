29cdd1c7dbc76ed32eb6a11916eb77d00c5ee913
112360125 <161788447+112360125@users.noreply.github.com>
2025-10-21 17:50:19 +0800

    Create Sample8.java

---
+++ Sample8.java
@@ -0,0 +1,14 @@
+public class Sample8
+{
+  public static void main(String[]args)
+  {
+    int num = 3;
+
+    System.out.println("變數num的值是"+num);
+
+    num = 5;
+
+    System.out.println("更新後變數num的值");
+    System.out.println("更新後變數num的值是"+num);
+  }
+}
