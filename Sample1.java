0ad55120615b52d426d6057d22540e08dac7fa43
112360125 <161788447+112360125@users.noreply.github.com>
2025-10-21 17:43:55 +0800

    Update Sample1.java

--- Sample1.java
+++ Sample1.java
@@ -1,8 +1,9 @@
 public class Smaple1
 {
-	public static void main(String[arg])
+	public static void main(String[]args)
 	{
 	  System.out.print("歡迎使用java!");
 	  System.out.print("歡迎使用java吧!");
 	}
+
 }
